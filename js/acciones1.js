9c91d2da54ff58075c1ab455bdee8c4e9a48e478
xanat11solorio <cb16.solorio.corio.xanat@gmail.com>
2018-05-23 13:47:54 -0500

    2subida

--- js/acciones1.js
+++ js/acciones1.js
@@ -2,22 +2,22 @@
 
 $(document).ready(function(e){
 	document .addEventListener ("deviceready",function(){
-		$('#disp table td').eq (3).text(device.model);
-		$('#disp table td').eq (5).text(device.cordova);
-		$('#disp table td').eq (7).text(device.platform);
-		$('#disp table td').eq (9).text(device.version);
-		$('#disp table td').eq (11).text(device.uuid);
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause", function(){
-		escribehistoria ('la app se pausó')
+		escribehistoria ('la app se pausó');
 	},false);//pause
 	document.addEventListener("resume", function(){
-		escribehistoria ('la app se reinició')
+		escribehistoria ('la app se reinició');
 	},false);//resume
 	document.addEventListener("online", function(){
-		escribehistoria ('la app se conecto a la red')
+		escribehistoria ('la app se conecto a la red');
 	},false);//se conecto
 	document.addEventListener("offline", function(){
-		escribehistoria ('la app se desconecto de la red')
+		escribehistoria ('la app se desconecto de la red');
 	},false);//se desconecto
 	
 	
@@ -27,5 +27,5 @@ $(document).ready(function(e){
 });//document
 
 function escribehistoria(accion){
-	$('#eHistoria').append('<li>'+'</li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
